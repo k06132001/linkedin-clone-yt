00f13853dd7d2707092f09c189639339ec6785d4
Dylan <k06132001@gmail.com>
2022-06-27 17:50:36 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "content.linkedin.com",
+      "rb.gy",
+      "static-exp1.licdn.com",
+      "www.iconsdb.com",
+    ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
